28cbd2efe13106cb11a5c1574eb70ef547d16ef7
whaapps <edi.cybereye@gmail.com>
2018-10-07 15:32:41 +0700

    update validasi

--- lib/main.dart
+++ lib/main.dart
@@ -14,36 +14,66 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   String nama, email, phone, company;
+  final _key = new GlobalKey<FormState>();
+
+  _check() {
+    final form = _key.currentState;
+    if (form.validate()) {
+      form.save();
+      print('$nama, $email, $phone, $company');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Container(
-        padding: EdgeInsets.all(16.0),
-        child: ListView(
-          children: <Widget>[
-            EditText(
-              errorLabel: 'Please fill your fullname',
-              label: 'Nama Lengkap',
-              onSaved: (a) => nama = a,
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            EditText(
-              errorLabel: 'Please fill your email address',
-              label: 'Email Address',
-              onSaved: (a) => email = a,
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            CustomButton(
-              label: 'Submit',
-              onPressed: () {},
-            ),
-            
-          ],
+      body: Form(
+        key: _key,
+        child: Container(
+          padding: EdgeInsets.all(16.0),
+          child: ListView(
+            children: <Widget>[
+              EditText(
+                errorLabel: 'Please fill your fullname',
+                label: 'Nama Lengkap',
+                onSaved: (a) => nama = a,
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              EditText(
+                errorLabel: 'Please fill your email address',
+                label: 'Email Address',
+                onSaved: (a) => email = a,
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              EditText(
+                errorLabel: 'Please fill your phone number',
+                label: 'No. Handphone',
+                onSaved: (a) => phone = a,
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              EditText(
+                errorLabel: 'Please fill your University or Company',
+                label: 'University or Company Name',
+                onSaved: (a) => company = a,
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              CustomButton(
+                label: 'Submit',
+                onPressed: () {
+                  _check();
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
